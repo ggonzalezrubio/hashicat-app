9c9ddf87057cbdedd33b0c2ec8232d06d6a3e8c7
ggonzalezrubio <ggrubio@outlook.com>
2024-05-01 20:08:00 +0000

    Added Backup Storage module

--- main.tf
+++ main.tf
@@ -202,8 +202,8 @@ resource "null_resource" "configure-cat-app" {
   }
 }
 
-module "backupstorage" {
-  source = "app.terraform.io/tf-org-ggr/azure-backup/azurerm"
-  version = "1.0.0"
-  resource_group_name = "${var.prefix}-private"
+module "azure-backup" {
+  source  = "ravensorb/azure-backup/azurerm"
+  version = "1.0.2"
+  resource_group_name = "${var.prefix}-public"
 }
